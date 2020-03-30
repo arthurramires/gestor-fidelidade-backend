4b3d9a123a623ce7fb691dcc05cdb56ac5d9f7e3
arthurramires <arthurramires1802@outlook.com>
2020-03-30 10:14:32 -0400

    Back-end para teste

---
+++ src/controllers/UserController.js
@@ -0,0 +1,26 @@
+const connection = require('../database/connection');
+const crypto = require('crypto');
+
+
+module.exports = {
+    async index(request, response){
+        const users = await connection('users').select('*');
+    
+        return response.json(users);
+    },
+    
+    async create(request, response){
+        const {name, email, password} = request.body;
+        
+        const id = crypto.randomBytes(4).toString('HEX');
+
+        await connection('users').insert({
+            id,
+            name,
+            email,
+            password,
+        })
+
+        return response.json({ id });
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+//Definindo porta para rodar o servidor
+app.listen(process.env.PORT || 3333);
